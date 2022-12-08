f214b647b95dd3351909dd6670e9ccaf69a8df16
manami-sato <manamisato2001@gmail.com>
2022-12-08 16:37:07 +0900

    commit

--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -7,10 +7,11 @@ import OriginalSpacer from './OriginalSpacer';
 type Props = {
   children: ReactNode;
   size: number;
+  unmove?: boolean;
 };
 
-const FadeIn: FC<Props> = ({ children, size }) => {
-  // const animation = useAnimation();
+const FadeIn: FC<Props> = ({ children, size, unmove = false }) => {
+  const animation = useAnimation();
   // const variants = {
   //   hidden: {
   //     opacity: 0,
@@ -23,26 +24,32 @@ const FadeIn: FC<Props> = ({ children, size }) => {
   //   },
   // };
 
-  // const { ref, inView } = useInView({
-  //   rootMargin: '-200px', // ref要素が現れてから50px過ぎたら
-  //   triggerOnce: true, // 最初の一度だけ実行
-  // });
+  const { ref, inView } = useInView({
+    rootMargin: '-200px', // ref要素が現れてから50px過ぎたら
+    triggerOnce: true, // 最初の一度だけ実行
+  });
 
-  // useEffect(() => {
-  //   if (inView) {
-  //     animation.start('visible');
-  //   }
-  // }, [animation, inView]);
+  useEffect(() => {
+    if (inView) {
+      animation.start('visible');
+    }
+  }, [animation, inView]);
 
   return (
     <>
       <OriginalSpacer size={size + 'px'} />
       <Box
-        // as={motion.div}
+        as={motion.div}
         // initial="hidden"
         // animate={inView && 'show'}
+        initial={{ opacity: 0 }}
+        // initial={{ opacity: 0, y: unmove ? 0 : 100 }}
+        // transition={{ duration: 0.5 + delay * delayBreakpoint }}
         // variants={variants}
-        // ref={ref}
+        // whileInView={{ opatity: 1 }}
+        // whileInView={{ opacity: 1, y: 0 }}
+        whileInView={{ opacity: 1 }}
+        ref={ref}
       >
         {children}
       </Box>
