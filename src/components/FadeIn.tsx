f255fcb56fa6629b9c3ffae238205ff8e1270ac6
manami-sato <manamisato2001@gmail.com>
2022-12-08 16:24:55 +0900

    commit

--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -10,39 +10,39 @@ type Props = {
 };
 
 const FadeIn: FC<Props> = ({ children, size }) => {
-  const animation = useAnimation();
-  const variants = {
-    hidden: {
-      opacity: 0,
-    },
-    show: {
-      opacity: 1,
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
+  // const animation = useAnimation();
+  // const variants = {
+  //   hidden: {
+  //     opacity: 0,
+  //   },
+  //   show: {
+  //     opacity: 1,
+  //     transition: {
+  //       duration: 0.2,
+  //     },
+  //   },
+  // };
 
-  const { ref, inView } = useInView({
-    rootMargin: '-200px', // ref要素が現れてから50px過ぎたら
-    triggerOnce: true, // 最初の一度だけ実行
-  });
+  // const { ref, inView } = useInView({
+  //   rootMargin: '-200px', // ref要素が現れてから50px過ぎたら
+  //   triggerOnce: true, // 最初の一度だけ実行
+  // });
 
-  useEffect(() => {
-    if (inView) {
-      animation.start('visible');
-    }
-  }, [animation, inView]);
+  // useEffect(() => {
+  //   if (inView) {
+  //     animation.start('visible');
+  //   }
+  // }, [animation, inView]);
 
   return (
     <>
       <OriginalSpacer size={size + 'px'} />
       <Box
-        as={motion.div}
-        initial="hidden"
-        animate={inView && 'show'}
-        variants={variants}
-        ref={ref}
+        // as={motion.div}
+        // initial="hidden"
+        // animate={inView && 'show'}
+        // variants={variants}
+        // ref={ref}
       >
         {children}
       </Box>
