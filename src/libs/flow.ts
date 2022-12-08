55d87b9824f716b683baec3d6ee8680436ac3438
manami-sato <manamisato2001@gmail.com>
2022-12-08 16:11:05 +0900

    commit

--- src/libs/flow.ts
+++ src/libs/flow.ts
@@ -3,36 +3,36 @@ export const flow: strengthType[] = [
   {
     title: '無料カウンセリング',
     text: '結婚相談所が初めてでもご安心ください。あなたの婚活状況を振り返りつつ、「精神行動分析」をベースに「どうしたら幸せな結婚ができるのか？」をご提案します。オンラインでのカウンセリングも承っております。',
-    img: 'https://glow-marriage.com/wp-content/themes/pink-glow/img/05_flow/flow01.jpg',
+    img: 'https://images.microcms-assets.io/assets/91ccec7b6d554ddcaee759c5cdf7b840/98d567fcabe5459c8c05237afdec3f2a/flow-01.jpg',
   },
   {
     title: 'プロフィール作成',
     text: '婚活で勝てるプロフィールを作ります。あなたの生い立ちや理想とする結婚生活と、あなたの好みも踏まえ、あなたが求めるお相手に刺さるプロフィールを作成するコンサルティングを実施します。',
-    img: 'https://glow-marriage.com/wp-content/themes/pink-glow/img/05_flow/flow02.jpg',
+    img: 'https://images.microcms-assets.io/assets/91ccec7b6d554ddcaee759c5cdf7b840/f1d998307cb94592b0a73f51d4a97ba9/flow-02.jpg',
   },
   {
     title: 'お相手探し',
     text: 'ちょっとしたスキマ時間を利用し、あなたが幸せになれる理想のパートナー選びをスマホやパソコンで行いましょう。GLOWでは、お見合いの申し込み人数に制限を設けておりません。',
-    img: 'https://glow-marriage.com/wp-content/themes/pink-glow/img/05_flow/flow03.jpg',
+    img: 'https://images.microcms-assets.io/assets/91ccec7b6d554ddcaee759c5cdf7b840/ef682e0f866344a7a5707f6d13c98bf7/flow-03.jpg',
   },
   {
     title: 'お見合い',
     text: 'あなたとお相手が「会ってみたい」となると「お見合い成立」。日時や場所の調整はGLOWにて行います。お見合いはZOOMを活用したオンラインorホテルのラウンジでお茶をしつつ1時間ほどお話を。',
-    img: 'https://glow-marriage.com/wp-content/themes/pink-glow/img/05_flow/flow04.jpg',
+    img: 'https://images.microcms-assets.io/assets/91ccec7b6d554ddcaee759c5cdf7b840/65b69a9f4581409bbe996837f1763153/flow-04.jpg',
   },
   {
     title: '仮交際',
     text: 'お見合い後「もう一度お会いしたい」となったら交際成立。このタイミングで連絡先を交換し、男性から女性へ電話をかけてデートのお約束をします。GLOWでは、アドバイザーが「交際を進めていくためのコツ」を伝授！',
-    img: 'https://glow-marriage.com/wp-content/themes/pink-glow/img/05_flow/flow05.jpg',
+    img: 'https://images.microcms-assets.io/assets/91ccec7b6d554ddcaee759c5cdf7b840/0be3bf7d663948488f08f42b959209ea/flow-05.jpg',
   },
   {
     title: '真剣交際',
     text: 'お互いに申し出いただきましたら真剣交際のスタート。金銭感覚、子供、仕事、ご両親のことなど、積極的に結婚に向き合いましょう。GLOWでは、適時適切なアドバイスと、結婚に向けてのすり合わせを致します。',
-    img: 'https://glow-marriage.com/wp-content/themes/pink-glow/img/05_flow/flow06.jpg',
+    img: 'https://images.microcms-assets.io/assets/91ccec7b6d554ddcaee759c5cdf7b840/d9be031735854d9dbca90e7161d15b95/flow-06.jpg',
   },
   {
     title: '成婚退会！',
     text: 'プロポーズをし、おふたりが同じ気持ちで歩みだすことが決まったら、ご成婚となります。GLOWでは、出会いから2〜6か月で自然に結婚に向けて気持ちが固まる方が殆ど。プロポーズの方法や両家顔合わせなど、お二人にとって幸せな未来へのアドバイスも行います。',
-    img: 'https://glow-marriage.com/wp-content/themes/pink-glow/img/05_flow/flow07.jpg',
+    img: 'https://images.microcms-assets.io/assets/91ccec7b6d554ddcaee759c5cdf7b840/de40dce4e4b945debd17c8dbbefd22b3/flow-07.jpg',
   },
 ];
