b1a140db6c8db94af1f037cd63beb8af1e60aeb4
manami-sato <manamisato2001@gmail.com>
2022-12-08 16:39:39 +0900

    commit

--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode, useEffect, useRef } from 'react';
-import { useInView } from 'react-intersection-observer';
+// import { useInView } from 'react-intersection-observer';
 import { Box, keyframes } from '@chakra-ui/react';
-import { motion, useAnimation } from 'framer-motion';
+// import { motion, useAnimation } from 'framer-motion';
 import OriginalSpacer from './OriginalSpacer';
 
 type Props = {
@@ -11,7 +11,7 @@ type Props = {
 };
 
 const FadeIn: FC<Props> = ({ children, size, unmove = false }) => {
-  const animation = useAnimation();
+  // const animation = useAnimation();
   // const variants = {
   //   hidden: {
   //     opacity: 0,
@@ -24,32 +24,32 @@ const FadeIn: FC<Props> = ({ children, size, unmove = false }) => {
   //   },
   // };
 
-  const { ref, inView } = useInView({
-    rootMargin: '-200px', // ref要素が現れてから50px過ぎたら
-    triggerOnce: true, // 最初の一度だけ実行
-  });
+  // const { ref, inView } = useInView({
+  //   rootMargin: '-200px', // ref要素が現れてから50px過ぎたら
+  //   triggerOnce: true, // 最初の一度だけ実行
+  // });
 
-  useEffect(() => {
-    if (inView) {
-      animation.start('visible');
-    }
-  }, [animation, inView]);
+  // useEffect(() => {
+  //   if (inView) {
+  //     animation.start('visible');
+  //   }
+  // }, [animation, inView]);
 
   return (
     <>
       <OriginalSpacer size={size + 'px'} />
       <Box
-        as={motion.div}
-        // initial="hidden"
-        // animate={inView && 'show'}
-        initial={{ opacity: 0 }}
-        // initial={{ opacity: 0, y: unmove ? 0 : 100 }}
-        // transition={{ duration: 0.5 + delay * delayBreakpoint }}
-        // variants={variants}
-        // whileInView={{ opatity: 1 }}
-        // whileInView={{ opacity: 1, y: 0 }}
-        whileInView={{ opacity: 1 }}
-        ref={ref}
+        // as={motion.div}
+        // // initial="hidden"
+        // // animate={inView && 'show'}
+        // initial={{ opacity: 0 }}
+        // // initial={{ opacity: 0, y: unmove ? 0 : 100 }}
+        // // transition={{ duration: 0.5 + delay * delayBreakpoint }}
+        // // variants={variants}
+        // // whileInView={{ opatity: 1 }}
+        // // whileInView={{ opacity: 1, y: 0 }}
+        // whileInView={{ opacity: 1 }}
+        // // ref={ref}
       >
         {children}
       </Box>
