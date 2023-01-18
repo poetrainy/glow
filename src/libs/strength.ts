d6e8901a6b276d35b751a87309ce5c36cff941b4
manami-sato <manamisato2001@gmail.com>
2023-01-18 14:44:02 +0900

    commit

--- src/libs/strength.ts
+++ src/libs/strength.ts
@@ -12,7 +12,7 @@ export const strength: strengthType[] = [
     img: 'https://images.microcms-assets.io/assets/91ccec7b6d554ddcaee759c5cdf7b840/c428e7ea605c43e3a7dd98bc5810d518/strength-02.png',
   },
   {
-    title: '担当者がトランスジェンダー当事者',
+    title: 'ライフスタイルに合わせた3つのプラン',
     text: 'お仕事・年齢などの、ご都合に合わせた3つのプランをご用意しております。自分に婚活は難しいかも…とお思いの方こそ、一度ご相談ください。',
     img: 'https://images.microcms-assets.io/assets/91ccec7b6d554ddcaee759c5cdf7b840/8ee9c60c3fce4d7093ca0c066749370b/strength-03.png',
   },
